d1ed3ffa95d9bfa01cf009001da1e7865b64228a
MauriDiPietro <dipietro.jm@gmail.com>
2025-08-15 11:17:04 -0300

    fix: importaciones

--- src/containers/landing/index.tsx
+++ src/containers/landing/index.tsx
@@ -411,7 +411,7 @@ const LandingPage: React.FC = () => {
           <Grid item xs={12} md={6}>
             <Box
               component="img"
-              src="/public/programador.jpg"
+              src="/public/programador.JPG"
               alt="Programador trabajando"
               sx={{
                 maxWidth: "100%",
